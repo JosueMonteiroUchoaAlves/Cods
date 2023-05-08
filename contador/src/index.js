2e03b2afe495ef89c1473bff00629d079973b69d
JosueMonteiroUchoaAlves <jmua@aluno.ifnmg.edu.br>
2023-05-08 20:47:03 -0300

    versão contador por funções

--- contador/src/index.js
+++ contador/src/index.js
@@ -4,8 +4,8 @@ import './App.css';
 import Contador from './App.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <html>
+  <>
     <Contador /> 
-  </html>
+  </>
 );
 
