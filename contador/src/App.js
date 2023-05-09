2fd3cd67e3bf0b3d9ff43106b3bceb94191286e6
JosueMonteiroUchoaAlves <jmua@aluno.ifnmg.edu.br>
2023-05-09 13:06:45 -0300

    COM FUNCOES (agora sim)

--- contador/src/App.js
+++ contador/src/App.js
@@ -1,44 +1,30 @@
 import './App.css'
-import React, { Component } from 'react';
+import {React, useState} from 'react';
+//Usei chaves para importar mais de um elemento de um mesmo local de uma vez só
+function Botoes(){
+    // A função useState retorna um array com uma variável e a função para modificar a variável (Apenas a função pode fazer isso)
+    const [contador, setContador] = useState(0);
 
-
-class Contador extends Component{ /*Component é uma classe do próprio react. Criei uma classe contador com base nela.*/
-    constructor(props){ /*Criei um método construtor para essa classe nova*/
-        super(props); /*Peguei as propriedades da classe Component, entre elas o método state*/
-        this.state = { /*No método state eu declaro todos os atributos da minha classe*/
-            contador: 0, /*Inicializei um atributo que será o contador com o valor inicial de 0*/
-        }
+    function clickPlus(){
+        setContador(contador+1);
     }
-    clickPlus = () => { //Criei uma função (método) chamado click+, para quando o botão for clicado haver um incremento
-        this.setState(prevState => ({ /*prevState pega o valor anterior de contador (poderia fazer do 
-                                        jeito mais simples mas dessa forma aqui eu tenho certeza que 
-                                        o valor que estou pegando é o último*/
-            contador: prevState.contador + 1,
-        }));
+    function clickMinus(){
+        setContador(contador-1);
     }
-    clickMinus = () => { //Criei uma função (método) chamado click-, para quando o botão for clicado haver decremento
-        this.setState(prevState => ({ // O setState acessa o atributo que eu colocar ali embaixo e atualiza o valor dele
-            contador: prevState.contador - 1 //Peguei o último valor de contador e fiz um decremento
-        }));
-    }
-    reset = () => {
-      this.setState({
-        contador: 0, // contador vai ser resetado
-      });
-    }
-    // Agora que já fiz as funções de incremento e decremento, posso "renderizar" para a 
-    render(){
-        return( //O uso de parênteses é para agrupar e envolver vários elementos em uma única expressão
-            <div>
-                <h1>Contador</h1>
-                <button className ="estilo-botoes" onClick = {this.clickPlus}>+</button>
-                <h6>{this.state.contador}</h6>
-                <button className="estilo-botoes" onClick = {this.clickMinus}>-</button>
-                <br/>
-                <br/>
-                <button className="estilo-botoes" onClick = {this.reset}>RESET</button>
-            </div>
-        );
-    }
+    return(
+        //O uso de parênteses é para agrupar e envolver vários elementos em uma única expressão
+        <div>
+            <h1>Contador</h1>
+            <button className ="estilo-botoes" onClick = {this.clickPlus}>+</button>
+            <h6>{this.state.contador}</h6>
+            <button className="estilo-botoes" onClick = {this.clickMinus}>-</button>    
+            <br/>
+            <br/>
+            <button className="estilo-botoes" onClick = {this.reset}>RESET</button>
+        </div>
+    )
 }
-export default Contador;
+
+// O uso de funções no lugar de classes deixa o código muito menor!
+
+export default Botoes;
