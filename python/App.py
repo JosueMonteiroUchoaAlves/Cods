2847c7b96573206385a034a9ac4aaa821605f810
unknown <jmua@aluno.ifnmg.edu.br>
2023-05-30 23:00:30 -0300

    feat: one route, one operation

--- python/App.py
+++ python/App.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
-from typing import Union
 
 app = FastAPI()
 
-
+# Aponta que a função logo abaixo dela será executada quando houver uma requisição
 @app.get("/")
-def read_root():
+def hello_world():
     return {"Hello": "World"}
+#Quando quiser somar
+@app.get("/soma")
+def soma(var1: int, var2: int):
+    return {"Soma":var1+var2} 
 
+@app.get("/subtracao")
+def subtracao(var1:int, var2:int):
+    return {"Diferença": var1-var2}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/divisao")
+def divisao(var1:int,var2:int):
+    return {"Quociente":var1/var2}
+
+@app.get("/multiplicacao")
+def multiplicacao(var1:int, var2: int):
+    return {"Produto":var1*var2}
